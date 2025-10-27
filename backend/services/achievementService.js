93d41ba7930742446e23ed8ddba03aaceb50015b
Giankarlo Rivera <signmein3456@gmail.com>
2025-10-27 15:38:33 +0800

    created achievement service

---
+++ backend/services/achievementService.js
@@ -0,0 +1,29 @@
+const achievement = require('../models/achievementModel');
+const userAchievement = require('../models/userAchievementModel');
+
+const achievementChecker = async (userId, criteria) => {
+    // Check if the achievement with the given criteria exists
+    const achievementRecord = await achievement.findOne({ criteria });
+    if (!achievementRecord) {
+        throw new Error('Achievement not found for the given criteria');
+    }
+    // Check if the user has already earned this achievement
+    const existingUserAchievement = await userAchievement.findOne({ 
+        userId,
+        achievementId: achievementRecord.id
+    });
+    if (existingUserAchievement) {
+        return { message: 'Achievement already earned' };
+    }
+    // Award the achievement to the user
+    const newUserAchievement = new userAchievement({
+        userId,
+        achievementId: achievementRecord.id
+    });
+    await newUserAchievement.save();
+    return { message: 'Achievement awarded', achievement: achievementRecord };
+}
+
+module.exports = {
+    achievementChecker
+};
