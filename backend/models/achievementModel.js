497fa0929dd2933e1b2303c9475d569115677e23
Giankarlo Rivera <signmein3456@gmail.com>
2025-10-27 15:34:22 +0800

    created achievement model

---
+++ backend/models/achievementModel.js
@@ -0,0 +1,12 @@
+const { v4: uuidv4 } = require('uuid');
+const mongoose = require('mongoose');
+
+const achievementSchema = new mongoose.Schema({
+    id: { type: String,  unique:true, default: () => uuidv4() },
+    name: { type: String, required: true },
+    desctiprion: { type: String, required: true },
+    badgeIconUrl: { type: String, required: true },
+    criteria: { type: String, required: true }
+})
+
+module.exports = mongoose.model('Achievement', achievementSchema);
