cf752982bfe82d4cec1304df9716edb5d459a515
Giankarlo Rivera <signmein3456@gmail.com>
2025-11-22 07:30:59 +0800

    added user exp, level, and badge array to user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     id: { type: String,  unique:true, default: () => uuidv4() },
     username: { type: String, required: true, trim: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    exp: { type: Number, default: 0, min: 0 },
+    level: { type: Number, default: 1, min: 1 },
+    badges: { type: [String], default: [] }
 })
 
 module.exports = mongoose.model('User', userSchema);
