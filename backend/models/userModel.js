32da12540161c5bf81bff0de66352c74e75d1827
hazzara <hjazur@gbox.adnu.edu.ph>
2025-11-25 12:43:17 +0800

    Add social attributes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,7 +4,15 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     id: { type: String,  unique:true, default: () => uuidv4() },
     username: { type: String, required: true, trim: true },
-    password: { type: String, required: true }
-})
+    password: { type: String, required: true },
+    following: [{ type: String }], 
+    followers: [{ type: String }],
+    publicProfile: { type: Boolean, default: true }
+}, {
+    timestamps: true
+});
+
+userSchema.index({ following: 1 });
+userSchema.index({ followers: 1 });
 
 module.exports = mongoose.model('User', userSchema);
