32863bc5525c3079d75b693b85309626274ea0f0
Azeri-ne <loudenhal@protonmail.com>
2025-12-12 10:10:12 +0800

    Remove comments

--- backend/services/achievementService.js
+++ backend/services/achievementService.js
@@ -2,12 +2,10 @@ const achievement = require('../models/achievementModel');
 const userAchievement = require('../models/userAchievementModel');
 
 const achievementChecker = async (userId, criteria) => {
-    // Check if the achievement with the given criteria exists
     const achievementRecord = await achievement.findOne({ criteria });
     if (!achievementRecord) {
         throw new Error('Achievement not found for the given criteria');
     }
-    // Check if the user has already earned this achievement
     const existingUserAchievement = await userAchievement.findOne({ 
         userId,
         achievementId: achievementRecord.id
@@ -15,7 +13,6 @@ const achievementChecker = async (userId, criteria) => {
     if (existingUserAchievement) {
         return { message: 'Achievement already earned' };
     }
-    // Award the achievement to the user
     const newUserAchievement = new userAchievement({
         userId,
         achievementId: achievementRecord.id
