4f45b5caf8d86d434ff9220fed62728c742ad5fa
Erice Matthew Isaiah Del Puerto Cama <ecama@gbox.adnu.edu.ph>
2025-12-12 18:52:34 +0800

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     id: { type: String,  unique:true, default: () => uuidv4() },
     username: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true }, /* Added this line */
     password: { type: String, required: true },
     following: [{ type: String }], 
     followers: [{ type: String }],
